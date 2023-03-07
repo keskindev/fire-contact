5260fadcd1c7b7b042333fbff7ead16b589a3f79
keskindev <=>
2023-03-07 15:33:42 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import FormComponent from './components/form/FormComponent';
+import Contacts from './components/contacts/Contacts';
+import { addInfo, updateInfo } from './utils/functions';
+import { ToastContainer } from 'react-toastify';
+
+const initialValues={username:"",phoneNumber:"",gender:"NO INFO"}
 
 function App() {
+  const [info,setInfo]=useState(initialValues)
+
+  const handleFormSubmit=()=>{
+    if(info.id){
+    updateInfo(info)}
+    else{
+      addInfo(info)
+    }
+  }
+
+  const editHandler=(id,username,phoneNumber,gender)=>{
+    setInfo({id,username,phoneNumber,gender});
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <FormComponent info={info} setInfo={setInfo} handleFormSubmit={handleFormSubmit} />
+     <Contacts editHandler={editHandler}/>
+     <ToastContainer />
     </div>
   );
 }
